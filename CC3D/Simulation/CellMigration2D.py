99f48afc845a6be40e9a14bb325aae371b33245c
bvillant <96221291+bvillant@users.noreply.github.com>
2021-12-24 12:11:49 -0800

    Add files via upload

---
+++ CC3D/Simulation/CellMigration2D.py
@@ -0,0 +1,26 @@
+from cc3d import CompuCellSetup
+
+from CellMigration2DSteppables import CellMigration2DSteppable
+
+CompuCellSetup.register_steppable(steppable=CellMigration2DSteppable(frequency=1))
+
+from CellMigration2DSteppables import PolarityEvolutionSteppable
+
+CompuCellSetup.register_steppable(steppable=PolarityEvolutionSteppable(frequency=1))
+
+from CellMigration2DSteppables import MMPSecretionSteppable
+
+CompuCellSetup.register_steppable(steppable=MMPSecretionSteppable(frequency=1))
+
+from CellMigration2DSteppables import ECMDegradationSteppable
+
+CompuCellSetup.register_steppable(steppable=ECMDegradationSteppable(frequency=1))
+
+from CellMigration2DSteppables import IdFieldVisualizationSteppable
+
+CompuCellSetup.register_steppable(steppable=IdFieldVisualizationSteppable(frequency=1))
+
+CompuCellSetup.run()
+
+        
+        
